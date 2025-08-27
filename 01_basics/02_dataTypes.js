7132540f8ec7a80e0fd2f129a373c77dabfaeeb9
dsr2000004-star <dsr2000004@gmail.com>
2025-08-27 19:56:35 +0000

    dataTypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -4,3 +4,22 @@
 
 console.log(3+3)
 console.log("Divyanshu")
+
+let name = "Divyanshu"
+let age = 18
+let isLoggedIn = false 
+
+// number => 2^53
+// bigint is for writing big numbers
+// string => ""
+// boolean => true/false
+// null => stand alone value 
+// undefined => abhi value defined nahi hui hai
+// symbol => unique 
+
+// object 
+
+console.log(typeof "Divyanshu", typeof age);
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
