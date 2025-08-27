cdc3922666d7fe08b2235a7b62c0d54b53e91157
dsr2000004-star <dsr2000004@gmail.com>
2025-08-27 20:28:05 +0000

    config donw

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score = null
+
+// const {score} = req.body sometimes it is not defined that what is actual dataType of variable
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let ValueInNumber = Number(score)
+console.log(typeof ValueInNumber);
+console.log(ValueInNumber);
+
+
+
+
