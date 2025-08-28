94f89ec123cbd5849f64286601a595092356fe1f
dsr2000004-star <dsr2000004@gmail.com>
2025-08-28 19:47:10 +0000

    conversion of datatype

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score = null
+let score = "divynashu"
 
 // const {score} = req.body sometimes it is not defined that what is actual dataType of variable
 
@@ -8,6 +8,29 @@ console.log(typeof(score));
 let ValueInNumber = Number(score)
 console.log(typeof ValueInNumber);
 console.log(ValueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = ""
+
+let aanewala = Boolean(isLoggedIn)
+
+console.log(typeof aanewala);
+console.log(aanewala);
+
+// 1 => true; 0 => false
+// "" => false
+// "Divyanshu" => true
+
+
+let someNumber  = 33
+let stringNumber  = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
 
 
 
