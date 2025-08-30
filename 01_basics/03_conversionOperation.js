06e16c18033e8b799f32eb3bd8337ef2e599676b
dsr2000004-star <dsr2000004@gmail.com>
2025-08-30 20:02:38 +0000

    container config done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -92,6 +92,16 @@ console.log(null > 0);
 console.log(null == 0);
 console.log(null >= 0);
 
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===
+
+console.log("2" === 2);                   
+
+
+
 
 
 
