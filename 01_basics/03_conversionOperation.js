44ed6afd50397a58df769b3f9db983d7ee203481
dsr2000004-star <dsr2000004@gmail.com>
2025-08-31 21:10:58 +0000

    container config done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -7,7 +7,7 @@ console.log(typeof(score));
 
 let ValueInNumber = Number(score)
 console.log(typeof ValueInNumber);
-console.log(ValueInNumber);
+console.log(ValueInNumber);    
 
 // "33" => 33
 // "33abc" => NaN
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,30 @@
+// Primitive 
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BignInt
+
+const score = 100 // (javascript is dynamically typed language as we do not have to describe type of variable)
+
+const isLoggedIn = false 
+const outsideTemp = null
+let userEmail; // undefined value deta hai 
+
+const id  = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+
+console.log();
+
+
+ 
+// Reference (Non Primitive)
+
+// Array, Objects, Function
+
+
+
+
+
+
+
+
