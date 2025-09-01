66005754ffd2de892ce0d6048e09b2439781906c
dsr2000004-star <dsr2000004@gmail.com>
2025-09-01 20:03:56 +0000

    datatype summary

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -3,6 +3,8 @@
 // 7 types: String, Number, Boolean, null, undefined, Symbol, BignInt
 
 const score = 100 // (javascript is dynamically typed language as we do not have to describe type of variable)
+const scoreValue = 100.3
+
 
 const isLoggedIn = false 
 const outsideTemp = null
@@ -10,16 +12,32 @@ let userEmail; // undefined value deta hai
 
 const id  = Symbol('123')
 const anotherId = Symbol('123')
-console.log(id === anotherId);
+console.log(id === anotherId);     
 
 
-console.log();
+// const bigNumber = 152682398472387467548976n
 
 
  
 // Reference (Non Primitive)
 
 // Array, Objects, Function
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+let myObj = {
+    name: Divyanshu, 
+    age: 21,
+}
+// const myfunction = function(){
+// console.log("Hello World");
+// }
+
+console.log(typeof bigNumber);
+
+
+
+
 
 
 
