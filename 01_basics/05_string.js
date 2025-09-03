abe9d7fe43c28bb352f6564a99546c9a1bf59272
dsr2000004-star <dsr2000004@gmail.com>
2025-09-03 21:18:52 +0000

    string done

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -5,3 +5,44 @@ console.log(name +  repoCount + " Value");
 
 console.log(`Hello my name is ${name} ans my repo count is ${repoCount}`);
 
+const gameName = new String('hitesh--hc')
+console.log(gameName);
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('hhc'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-9, 5)
+console.log(anotherString);
+
+const newStringOne = "           divyanshu         "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+console.log(newStringOne.trimStart());
+console.log(newStringOne.trimEnd());
+const url = "https://hitesh.com/hitesh%20choudhary"
+console.log(url.replace('%20', '-'));
+
+let a = "divyanshutuku" 
+console.log(a.slice(-11, 6)); // in this type -1 refers to last alphapet of word
+
+
+
+
+
+
+
+
+
+
+
+
+
+
