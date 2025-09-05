c259150a9ea51e62a8fb4efba118cbb9fcf0d97a
dsr2000004-star <dsr2000004@gmail.com>
2025-09-05 20:37:24 +0000

    nums abd maths done

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -24,6 +24,21 @@ console.log(Math.round(4.9));
 console.log(Math.ceil(4.1));
 console.log(Math.floor(4.99));
 console.log(Math.sqrt(49));
+console.log(Math.min(4, 3, 6, 8));
+console.log(Math.max(4, 3, 6, 8));
+console.log(Math.random());
+
+
+console.log((Math.random()*10) + 1);  // Very Important 
+
+const min = 10
+const max = 20
+
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
 
 
 
