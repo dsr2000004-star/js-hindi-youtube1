d5d7e3537ea28af0de7deeec97e6625c82029516
dsr2000004-star <dsr2000004@gmail.com>
2025-09-08 16:55:29 +0000

    array part one done

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -5,22 +5,56 @@ console.log(myArr[1]);
 
 // Array methods
 
-myArr.push(6)
-myArr.push(7)
-myArr.pop()
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
 console.log(myArr);
 
-myArr.unshift(9)
-myArr.shift()
+// myArr.unshift(9)
+// myArr.shift()
 
 console.log(myArr.includes(5));
 console.log(myArr.indexOf(4));
 console.log(myArr.indexOf(9));
 console.log(myArr.indexOf(14));
 
-
+const newArr = myArr.join()
 
 console.log(myArr);
+console.log(newArr);
+console.log(typeof newArr);
+
+// slice, splice (very important)
+
+console.log("A ", myArr);
+const myn1 = myArr.slice(1, 3)
+console.log(myn1);
+
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
+
+
+const a = ["akkad", "bakkad", "bambe", "bo", "assi", "nubbhe"]
+
+console.log(a.slice(2, 4));
+console.log(a);
+
+console.log(a.splice(2, 4));
+console.log(a);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
