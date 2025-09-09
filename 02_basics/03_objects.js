543af80fc98d6e4b8abfe25ede520e0c607b1ea9
dsr2000004-star <dsr2000004@gmail.com>
2025-09-09 20:50:25 +0000

    object 1

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -5,28 +5,51 @@
 Object.create
 
 //objects literals 
+
+const mySym = Symbol("key1")
+
+
 const JsUser = {
-    name: "Divynashu", 
+    name: "Divyanshu",
     "full name": "Divyanshu Singh Rawat",
-    age: 18, 
+    [mySym]: "myKey1",
+    
+    age: 18,
     location: "Jaipur",
     email: "divyanshu@google.com",
     isLoggedIn: "false",
     lastLoginDays: ["Monday", "Saturday"]
-    }
+}
 
 
-    // Two methods to see objects key and elements 
-    console.log(JsUser.email);
-    console.log(JsUser["full name"]);
-    
-    
+// Two methods to see objects key and elements 
+// console.log(JsUser.email);
+// console.log(JsUser["full name"]);
+// console.log(JsUser.mySym);
+// console.log(typeof JsUser.mySym);
+// console.log(JsUser[mySym]);
 
 
+JsUser.email = "divynashu@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "divyanshu@microsoft.com"
+// console.log(JsUser);
 
+JsUser.greeting = function(){
+console.log("Hello JS user");
 
+}
 
-    
+JsUser.greetingTwo = function(){
+console.log(`Hello JS user, ${this.name}`);
+
+}
+
+console.log(JsUser.greeting);
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
+
 
 
 // myArray = ["h", "i"]
