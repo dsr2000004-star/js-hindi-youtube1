c45539d99aac050e3d968fe3187b51ef6768ef6f
dsr2000004-star <dsr2000004@gmail.com>
2025-09-09 16:41:48 +0000

    object

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,33 @@
+// singleton
+
+
+// objects constructor
+Object.create
+
+//objects literals 
+const JsUser = {
+    name: "Divynashu", 
+    "full name": "Divyanshu Singh Rawat",
+    age: 18, 
+    location: "Jaipur",
+    email: "divyanshu@google.com",
+    isLoggedIn: "false",
+    lastLoginDays: ["Monday", "Saturday"]
+    }
+
+
+    // Two methods to see objects key and elements 
+    console.log(JsUser.email);
+    console.log(JsUser["full name"]);
+    
+    
+
+
+
+
+
+    
+
+
+// myArray = ["h", "i"]
+// console.log(myArray[1]);
