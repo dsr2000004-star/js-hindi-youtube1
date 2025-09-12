d5e5fe29621cce6099da790cc720e00be4d595cf
dsr2000004-star <dsr2000004@gmail.com>
2025-09-12 16:59:08 +0000

    function 1

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -8,3 +8,34 @@ console.log("H");
 }
 
 sayMyName()
+
+function addTwoNumbers(number1, number2){
+    console.log(number1 + number2);
+    
+
+}
+
+addTwoNumbers(3, 5)
+// addTwoNumbers("3", "4")
+// addTwoNumbers(3, "4")
+// addTwoNumbers(3, "a")
+// addTwoNumbers(3, null)
+
+const result  = addTwoNumbers(3, 5)
+console.log("Result:", result);
+
+
+// **********return value ka concept**********
+
+function foo(n1, n2){
+    let result1 = n1 + n2
+    console.log("Divyanshu");
+
+    return result1
+    // console.log("Divyanshu"); (Ye print nahi hoyega kyuki ye return ke baad likha hai)
+    
+
+}
+
+const result1 = foo(10, 20)
+console.log("The sum of these numbers is", result1);
