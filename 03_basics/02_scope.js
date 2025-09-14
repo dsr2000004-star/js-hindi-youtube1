316d2927a0c7a61de9318efa880812fa3d034dcf
dsr2000004-star <dsr2000004@gmail.com>
2025-09-14 20:14:24 +0000

    scope 1

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,26 @@
+// let a = 10
+// const b = 20
+// var c = 30
+
+
+
+if(true){
+let a = 10
+const b = 20
+var c = 30
+}
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+
+
+let kancha = 300
+
+if(true){
+let kancha = 21
+console.log("INNER: ", kancha);
+
+}
+
+console.log(kancha);
