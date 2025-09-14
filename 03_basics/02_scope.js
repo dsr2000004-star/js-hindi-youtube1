14e6f34a7f621a456e1e23e737e568309d0a0ad5
dsr2000004-star <dsr2000004@gmail.com>
2025-09-14 21:16:52 +0000

    video 22 done

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -24,3 +24,69 @@ console.log("INNER: ", kancha);
 }
 
 console.log(kancha);
+
+
+
+// ************************ Video - 22 **************************************
+
+
+function one (){
+    const username = "Divyanshu"
+
+
+    function two() {
+        const website = "Youtube"
+        console.log(username);
+        
+    }
+
+    // console.log(website);
+
+    // two()
+    
+}
+
+one()
+
+
+if (true){
+
+
+const username = "Divyanshu"
+
+if(username === "Divyanshu"){
+   const website = " youtube"
+   console.log(username + website);
+   
+}
+
+// console.log(website);
+
+
+}
+
+// console.log(username)
+
+
+// ********************** Important(Hoisting) ************************************
+
+console.log(addOne(5));
+
+function addOne(num){
+return num + 1
+}
+
+// console.log(addOne(5));
+
+
+
+
+
+console.log(addTwo(5));
+const addTwo = function(num){
+    return num + 2
+}
+// console.log(addTwo(5));
+
+
+
