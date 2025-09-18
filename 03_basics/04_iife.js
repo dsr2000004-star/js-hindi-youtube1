d4fbfa9eeb6495286b40cac223acf44db163f615
dsr2000004-star <dsr2000004@gmail.com>
2025-09-18 20:07:52 +0000

    IIFE

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,21 @@
+/////  IIFE (Immediately Invoked Function Expressions) ////////
+
+(function chai() {
+    console.log(`DB CONNECTED`);
+
+})();
+
+(function aurCode() {
+    console.log(`DB`);
+
+})()
+
+
+    (function aurCode(naam) {
+        console.log(`DB ${naam}`);
+
+    })("mera naam divyanshu hai")
+
+
+
+/* agar 2 IIFE functions ko run karwana hai to pehle IIFE function k baad ek comma (,) lagana hota hai varna dono hi run nahi karenge */
