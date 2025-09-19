3d872c2e47cfd0585ffaf8b873cedbce3621640a
dsr2000004-star <dsr2000004@gmail.com>
2025-09-19 16:50:28 +0000

    JSEC

---
+++ 03_basics/05_jsec.js
@@ -0,0 +1,9 @@
+let val1 = 10
+let val2 = 5
+function addNum(num1, num2) {
+    let total = num1 + num2
+    return total
+}
+
+let result1 = console.log(addNum(val1, val2))
+let result2 = console.log(addNum(10, 2))
