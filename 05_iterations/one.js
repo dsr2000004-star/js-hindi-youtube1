cdb645c72aa61af5796263ac9665422074f25c57
dsr2000004-star <dsr2000004@gmail.com>
2025-09-23 17:17:18 +0000

    iterations

---
+++ 05_iterations/one.js
@@ -0,0 +1,35 @@
+// for 
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+ if(element == 5){
+//   console.log("5 is best number");
+  
+ }
+
+    // console.log(element);
+    
+    
+}         // shortcut for duplicate Ctrl + D
+
+
+// console.log(element);
+
+
+for (let i = 0; i <= 10; i++) {
+   
+    console.log(`Outer loop value ${i}`);
+    for (let j = 0; j <= 10; j++) {
+        
+        console.log(`Inner loop value ${j} and outer loop  value ${i}`);
+        
+        
+    }
+    
+    
+}
