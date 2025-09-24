73bf49f8ca5cc046f64ef17ec813f6d211b14f66
dsr2000004-star <dsr2000004@gmail.com>
2025-09-24 20:51:38 +0000

    for of

---
+++ 05_iterations/three.js
@@ -0,0 +1,37 @@
+// for of 
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+
+// for (const element of object) {
+    
+// }
+
+let arr = ["mai", "chalu", "pichadi", "sab"]
+
+
+for (const value of arr) {
+    console.log(value);
+    
+}
+
+let greetings = "Hello World"
+
+for (const value of greetings) {
+
+if(value == greetings.charAt(5)){
+    
+
+    console.log("-");
+    continue
+    
+
+}
+    console.log(`print ${value}`);
+    
+}
+
+// Maps 
+
+const map = new Map()
