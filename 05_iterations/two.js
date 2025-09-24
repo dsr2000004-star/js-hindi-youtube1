e0a666ee4c1c2b128c01bcf59c92396cedeac77c
dsr2000004-star <dsr2000004@gmail.com>
2025-09-24 20:17:03 +0000

    do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,29 @@
+// while (condition) {
+    
+// }
+
+let index = 0
+while (index <= 10) {
+    console.log(`Value of index is ${index}`);
+    index = index + 2
+    
+}
+
+let myArray = ["duniya", "chale", "agaadi", "to"]
+let arr = 0
+while (arr < myArray.length) {
+    console.log(myArray[arr]);
+
+    arr = arr + 1
+    
+}
+
+// let score = 1;
+let score = 22
+
+do {
+    console.log(`Value of score is ${score}`);
+    score++
+    
+    
+} while (score <= 10);
