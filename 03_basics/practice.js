f5fc811d83303e82ad6157404d046420f5144b39
dsr2000004-star <dsr2000004@gmail.com>
2025-10-14 21:23:42 +0000

    practice.js

---
+++ 03_basics/practice.js
@@ -0,0 +1,38 @@
+// 
+function sayAdd(num1, num2) {
+    console.log(num1 + num2);
+    
+}
+
+sayAdd(3, 5)
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+
+    for (let j = 0; j <= 10; j++) {
+        // console.log(j);
+        
+       console.log(i + '+' + j + "=" + (i+j) );
+       
+        
+    }
+    
+    
+}
+
+
+function meraNaam(a, b) {
+
+    
+    console.log("d");
+    
+    return  a + b ;
+     
+    
+}
+
+const result = meraNaam(3, 10)
+
+console.log("Hamara Result aaya hai: ", result );
+
+
