f2fb5ec2aaee80843066ab38c0a6569523d8ee84
dsr2000004-star <dsr2000004@gmail.com>
2025-10-24 16:38:27 +0000

    projectstart.js

--- 07_projects/chaiaurcode.js
+++ 07_projects/chaiaurcode.js
@@ -4,8 +4,12 @@ const body = document.querySelector('body');
 
 buttons.forEach((button) => {
   console.log(button);
-  button.addEventListener('click', function (e) {
-    console.log(e);
-    console.log(e.target);
-  });
+  button.addEventListener('click', function(e){
+   console.log(e)
+   console.log(e.target)
+if(e.target.id==='grey'){
+body.style.backgroundColor = "grey"
+}
+
+  })
 });
---
+++ 07_projects/practice.js
@@ -0,0 +1,10 @@
+let month = 4
+switch (month) {
+  case 4:
+    console.log("April");
+    
+    break;
+
+  default:
+    break;
+}
