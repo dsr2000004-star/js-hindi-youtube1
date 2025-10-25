39b7f0d9e1b17e3e9ecabf480f5480e88d1911b0
dsr2000004-star <dsr2000004@gmail.com>
2025-10-25 09:46:02 +0000

    projecttwo.js

--- 07_projects/chaiaurcode.js
+++ 07_projects/chaiaurcode.js
@@ -25,13 +25,25 @@ buttons.forEach((button) => {
 
 // Project - 2
 
+document.querySelector('form')
+
+// this use case will give you empty value 
+// const height = parseInt(document.querySelector('#height'))
+
+
 form.addEventListener( 'submit', function(e){
-e.preventDefault()
+  e.preventDefault()
+  
+  const height = parseInt(document.querySelector('#height').value)
+  
+  const weight = parseInt(document.querySelector('#weight').value)
+  
+  const results = document.querySelector('#results')
 
-const height = parseInt(document.querySelector('#height'))
+  if(height === "" || height < 0 || isNaN(height)){
+    results.innerHTML = "Please enter a valid height"
+  }
 
-const weight = parseInt(document.querySelector('#weight'))
-
-const results = document.querySelector('#results')
-
-} )
+  results.innerHTML = `${height}` //  is project me agar number 888aa is form me likha hai to vo parint ki wajah se 888 ho jaayega
+  
+  } )
